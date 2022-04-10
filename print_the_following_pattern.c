646bcb36ebb00c7268773ed37e5666a10f3c7964
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-04-10 14:42:49 +0530

    #include <stdio.h>
    int main() {
      // size of pyramid
      // can also be read from stdin
      int size ;
      scanf("%d",&size);
      for (int i = 0; i < size; i++) {
        // printing spaces
        for (int j = 0; j < size-i-1; j++) {
          printf(" ");
        }
        // printing numbers
        for (int k = 0; k < 2*i+1; k++) {
          printf("%d", i+1);
        }
        printf("
    ");
      }
      return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
-int main()
-{
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=n;i>=1;i--)
-    {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
-        printf("
-");
+#include <stdio.h>
+
+int main() {
+  // size of pyramid
+  // can also be read from stdin
+  int size ;
+  scanf("%d",&size);
+  for (int i = 0; i < size; i++) {
+    // printing spaces
+    for (int j = 0; j < size-i-1; j++) {
+      printf(" ");
     }
+    // printing numbers
+    for (int k = 0; k < 2*i+1; k++) {
+      printf("%d", i+1);
+    }
+    printf("
+");
+  }
+  return 0;
 }
