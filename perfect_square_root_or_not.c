723f597c7639b2cab88d4e4413ca8a6db890b5fd
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-04-13 11:52:03 +0530

    #include<stdio.h> #include<math.h> int main() {     int s,n;     scanf("%d",&n);     s= (int)sqrt(n);     if(s*s==n)     {         printf("True");     }     else     {         printf("False");     }
    }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int s,n;
+    scanf("%d",&n);
+    s= (int)sqrt(n);
+    if(s*s==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    
+}
