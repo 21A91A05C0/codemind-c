0b5d1f88665851553d67c77a0005b21307d12621
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-04-16 10:30:17 +0530

    #include<stdio.h> int main() {     int n,flag=0;     scanf("%d",&n);     while(n!=1)     {         if(n%2==0)         {             n=n/2;         }         else if(n%3==0)         {             n=n/3;         }         else if(n%5==0)         {             n=n/5;         }         else         {             printf("Not Ugly Number");             flag=1;             break;         }     }     if(flag==0)     {         printf("Ugly Number");     }
    }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,31 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    int c=0,n;
+    int n,flag=0;
     scanf("%d",&n);
     while(n!=1)
     {
         if(n%2==0)
         {
-            n/=2;
+            n=n/2;
         }
         else if(n%3==0)
         {
-            n/=3;
+            n=n/3;
         }
-        else if(n%7==0)
+        else if(n%5==0)
         {
-            n/=7;
+            n=n/5;
         }
         else
         {
             printf("Not Ugly Number");
-            c=1;
+            flag=1;
             break;
         }
     }
-    if(c==0)
+    if(flag==0)
     {
         printf("Ugly Number");
     }
+    
 }
