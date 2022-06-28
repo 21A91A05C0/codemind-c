f3fff7e29ed3aea46d5b07f34fc2f038ecbac78f
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-06-28 15:47:23 +0530

    #include<stdio.h> int main() {     int n,m,i,j,sum=0,arr[1000][1000];     scanf("%d %d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             sum=sum+arr[i][j];         }
        }
        printf("%d",sum);
    }

--- Sum_of_all_elements_in_a_matrix.c
+++ Sum_of_all_elements_in_a_matrix.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int arr[20][20],r,c,i,j,sum=0;
-    scanf("%d%d",&r,&c);
-    for(i=0;i<r;i++)
+    int n,m,i,j,sum=0,arr[1000][1000];
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<m;j++)
         {
             scanf("%d",&arr[i][j]);
-            
         }
     }
-    for(i=0;i<r;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<m;j++)
         {
             sum=sum+arr[i][j];
-            
         }
+       
     }
     printf("%d",sum);
 }
